17a65ba21f88f10e653299a6a1dc8416f614c834
boggalajanu07-ux <boggalajanu07@gmail.com>
2026-03-20 15:36:34 +0530

    Improved calculator with loop and error handling

--- calculator.py
+++ calculator.py
@@ -1,22 +1,25 @@
 while True:
-    a = int(input("enter the first number:"))
-    b = int(input("enter the second number:"))
-    op=input("enter the operation (+,-,*,/):")
-    if op=="+":
+    a = float(input("enter the first number:"))
+    b = float(input("enter the second number:"))
+    print("choose operation:")
+    print("1.Add")
+    print("2.Subtract")
+    print("3.Multiply")
+    print("4.Divide")
+    choice = input("enter your choice(1/2/3/4):")
+    if choice == "1":
         print(a+b)
-    elif op=="-":
+    elif choice == "2":
         print(a-b)
-    elif op=="*":
+    elif choice == "3":
         print(a*b)
-    elif op=="/":
-        print(a/b)
-    elif op=="/":
-         if b==0:
-             print("cannot divided by zero")
+    elif choice == "4":
+         if b!=0:
+             print("a/b")
          else:
-             print(a/b)
+             print("cannot divisible by zero")
     else:
           print("invalid operation")
-    choice=input("do you want to continue? (y/n)")
-    if choice=="n":
+    again =input("do you want to continue? (y/n)")
+    if again =="n":
         break
